ef1e2a142557f9eccaa63b42a29f0655f115c9df
jinukee <jinukee333@gmail.com>
2025-12-04 17:16:29 +0900

    feat : marker 요청 api 주소 변경

--- src/main/java/com/home/infrastructure/web/map/MapController.java
+++ src/main/java/com/home/infrastructure/web/map/MapController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/map")
+@RequestMapping("/api/v1/map")
 @RequiredArgsConstructor
 @Slf4j
 public class MapController {
