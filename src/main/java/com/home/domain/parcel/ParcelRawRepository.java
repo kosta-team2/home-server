cf58f93face9eb53d574d1dc92756870fb02753a
kwongwangjae <kgj9906@naver.com>
2025-12-12 11:09:22 +0900

    fix: Parcel 토지 좌표 구현

---
+++ src/main/java/com/home/domain/parcel/ParcelRawRepository.java
@@ -0,0 +1,6 @@
+package com.home.domain.parcel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParcelRawRepository extends JpaRepository<ParcelRaw, String> {
+}
