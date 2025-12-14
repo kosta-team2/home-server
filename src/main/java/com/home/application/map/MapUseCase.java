fed94d42089eca969249b8b4993c1db139015a26
jinukee <jinukee333@gmail.com>
2025-12-14 21:46:26 +0900

    feat : 단지 Marker 표시 정보를 최근 실거래가와 총 세대수로 변경
    - 변경 사항에 따른 dto 변경
    - 변경 사항에 따른 repository 쿼리 변경

--- src/main/java/com/home/application/map/MapUseCase.java
+++ src/main/java/com/home/application/map/MapUseCase.java
@@ -5,9 +5,9 @@ import com.home.domain.region.RegionLevel;
 import com.home.domain.region.RegionRepository;
 import com.home.global.exception.ErrorCode;
 import com.home.global.exception.external.MapApiException;
-import com.home.infrastructure.web.map.dto.ComplexMarkersResponse;
 import com.home.infrastructure.web.map.dto.MarkersRequest;
 
+import com.home.infrastructure.web.map.dto.ParcelMarkerResponse;
 import com.home.infrastructure.web.map.dto.RegionMarkersResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -40,9 +40,7 @@ public class MapUseCase {
 				req.neLat(), req.neLng()));
 	}
 
-	public List<ComplexMarkersResponse> getComplexesByBoundary(MarkersRequest req) {
-		return ComplexMarkersResponse.fromParcelAgg(
-			parcelRepository.findParcelMarkersByBoundary(req.swLat(), req.swLng(), req.neLat(), req.neLng())
-		);
+	public List<ParcelMarkerResponse> getComplexesByBoundary(MarkersRequest req) {
+		return parcelRepository.findParcelMarkersByBoundary(req.swLat(), req.swLng(), req.neLat(), req.neLng());
 	}
 }
---
+++ src/main/java/com/home/infrastructure/web/map/dto/ParcelMarkerResponse.java
@@ -0,0 +1,9 @@
+package com.home.infrastructure.web.map.dto;
+
+public record ParcelMarkerResponse(
+	Long parcelId,
+	Double latitude,
+	Double longitude,
+	Long latestDealAmount,
+	Long unitCntSum
+) {}
