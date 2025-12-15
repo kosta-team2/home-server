671714841d06b2e7795b3d7dc4dc27372f345223
MINYONG PARK <parkminyong22@gmail.com>
2025-12-15 10:19:52 +0900

    거래내역 API 추가 및 기존 코드 수정 (#21)
    * feat: 거래 목록 api 추가
    
    * fix: 변수명 수정 및 api 수정
    
    * refactoring: 이중 for문에서 쿼리로 변경

--- src/main/java/com/home/application/detail/DetailUseCase.java
+++ src/main/java/com/home/application/detail/DetailUseCase.java
@@ -9,7 +9,11 @@ import com.home.domain.complex.Complex;
 import com.home.domain.complex.ComplexRepository;
 import com.home.domain.parcel.Parcel;
 import com.home.domain.parcel.ParcelRepository;
+import com.home.domain.trade.Trade;
+import com.home.domain.trade.TradeRepository;
+import com.home.infrastructure.web.detail.dto.ChartFilterRequest;
 import com.home.infrastructure.web.detail.dto.DetailResponse;
+import com.home.infrastructure.web.detail.dto.TradeResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,18 +24,41 @@ import lombok.extern.slf4j.Slf4j;
 public class DetailUseCase {
 	private final ParcelRepository parcelRepository;
 	private final ComplexRepository complexRepository;
+	private final TradeRepository tradeRepository;
 
 	@Transactional(readOnly = true)
-	public DetailResponse findByParcelId(Long id) {
-		Parcel parcel = parcelRepository.findById(id)
+	public DetailResponse findDetailByParcelId(Long parcelId) {
+		Parcel parcel = parcelRepository.findById(parcelId)
 			.orElseThrow(RuntimeException::new);	// todo 예외처리 not found
 
-		List<Complex> complexes = complexRepository.findAllByParcel_Id(id);
+		List<Complex> complexes = complexRepository.findAllByParcel_Id(parcelId);
 		if (complexes.isEmpty()) throw new RuntimeException();	// todo 예외처리 not found
 
 		// 1개의 parcel에 여러 complex가 존재시 complex를 제외하고 보낸다.
 		if (complexes.size() != 1) return DetailResponse.from(parcel);
 		return DetailResponse.of(parcel, complexes.get(0));
+	}
+
+	@Transactional(readOnly = true)
+	public TradeResponse findAllTradeByParcelId(Long parcelId) {
+		List<Long> complexIds = complexRepository.findAllIdsByParcel_Id(parcelId);
+		if (complexIds.isEmpty()) { throw new IllegalArgumentException(); }	//todo 예외처리
+		log.info("complexIds: {}", complexIds);
+
+		List<Trade> trades = tradeRepository.findByComplex_IdIn(complexIds);
+
+		return TradeResponse.from(parcelId, trades);
+	}
+
+	@Transactional(readOnly = true)
+	public TradeResponse findAllFilterdTradeByParcelId(Long parcelId, ChartFilterRequest request) {
+		List<Long> complexIds = complexRepository.findAllIdsByParcel_Id(parcelId);
+		if (complexIds.isEmpty()) { throw new IllegalArgumentException(); }	//todo 예외처리
+		log.info("complexIds: {}", complexIds);
+
+		List<Trade> trades = tradeRepository.findFilteredTradeByComplex_IdIn(complexIds, request.startDate(), request.endDate(), request.exclArea());
+
+		return TradeResponse.from(parcelId, trades);
 	}
 
 }
--- src/main/java/com/home/domain/trade/TradeRepository.java
+++ src/main/java/com/home/domain/trade/TradeRepository.java
@@ -1,6 +1,26 @@
 package com.home.domain.trade;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
+
+	List<Trade> findByComplex_Id(Long complexId);
+
+	List<Trade> findByComplex_IdIn(List<Long> complexIds);
+
+	@Query("""
+SELECT t
+FROM Trade t
+WHERE t.complex.id IN :complexIds
+AND
+t.exclArea = :exclArea
+AND
+t.dealDate BETWEEN :startDate AND :endDate
+""")
+	List<Trade> findFilteredTradeByComplex_IdIn(List<Long> complexIds, LocalDate startDate, LocalDate endDate, Double exclArea);
+
 }
