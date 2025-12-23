0949d86fcf3b9a815e808f39758ccfa05fea091a
kwongwangjae <kgj9906@naver.com>
2025-12-23 09:58:32 +0900

    fix:flywayV5

--- src/main/resources/db/migration/V5_2__create_new_indexes_concurrently.sql
+++ src/main/resources/db/migration/V5_2__create_new_indexes_concurrently.sql
@@ -1,8 +1,3 @@
-DROP INDEX IF EXISTS idx_trade_alive_complex_dealdate_id_desc;
-DROP INDEX IF EXISTS idx_complex_alive_parcel_id;
-DROP INDEX IF EXISTS idx_complex_alive_parcel_use_date;
-
---  대체 인덱스 생성
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_trade_latest_by_complex
     ON trade (complex_id, deal_date DESC, id DESC)
     INCLUDE (deal_amount, excl_area)
