afda2a0ac23b4c89365d147b653f40af47a5418d
kwongwangjae <kgj9906@naver.com>
2025-12-30 14:56:24 +0900

    fix

--- src/main/java/com/home/application/rankings/RankingUseCase.java
+++ src/main/java/com/home/application/rankings/RankingUseCase.java
@@ -3,6 +3,7 @@ package com.home.application.rankings;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.home.domain.rangkings.TradeTopPriceRepository;
 import com.home.domain.rangkings.TradeTopVolumeRepository;
@@ -17,12 +18,14 @@ public class RankingUseCase {
 	private final TradeTopPriceRepository topPriceRepository;
 	private final TradeTopVolumeRepository topVolumeRepository;
 
+	@Transactional(readOnly = true)
 	public List<TopPriceRankResponse> getTopPrice() {
 		return TopPriceRankResponse.from(
 			topPriceRepository.findAll()
 		);
 	}
 
+	@Transactional(readOnly = true)
 	public List<RegionTopVolumeRankResponse> getTopVolume() {
 		return RegionTopVolumeRankResponse.from(
 			topVolumeRepository.findAll()
